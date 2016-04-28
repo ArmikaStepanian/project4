93f1290cb0b5b9f371228ba505d71ff5c6217e4f
Stepanian <armikastepanian@gmail.com>
2016-04-28 17:08:47 +0600

    28.04.2016

--- src/main/java/ru/stepanian/project4/product/helper/CollectionHelper.java
+++ src/main/java/ru/stepanian/project4/product/helper/CollectionHelper.java
@@ -1,6 +1,7 @@
 package ru.stepanian.project4.product.helper;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import ru.stepanian.project4.entities.Category;
 import ru.stepanian.project4.entities.Colors;
 import ru.stepanian.project4.entities.Feature;
@@ -12,28 +13,25 @@ import java.util.List;
  * Created by Stepanian on 28.04.2016.
  */
 
+@Component
 public class CollectionHelper {
 
     @Autowired
     private ProjectService projectService;
 
-    public List<Colors> colors;
-    public List<Category> categories;
-    public List<Feature> features;
+    private List<Colors> colors = projectService.listColors();
+    private List<Category> categories = projectService.listCategories();
+    private List<Feature> features = projectService.listFeatures();
 
-    public List<Colors> getColors(){
-        if (colors==null)
-            return projectService.listColors();
+    public List<Colors> getColors() {
         return colors;
     }
-    public List<Category> getCategories(){
-        if (categories==null)
-            return projectService.listCategories();
+
+    public List<Category> getCategories() {
         return categories;
     }
-    public List<Feature> getFeatures(){
-        if (features==null)
-            return projectService.listFeatures();
+
+    public List<Feature> getFeatures() {
         return features;
     }
 }
